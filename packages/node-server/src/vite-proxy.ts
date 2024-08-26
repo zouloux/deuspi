6af4a310b711ba1e879c9409c4ff41884eeadc74
Alexis Bouhet <alexis@les-hallucines.studio>
2024-08-26 17:24:50 +0200

    @zouloux/node-server - v1.3.0 : New and more powerfull serveFrontEnd fastify static utility

--- packages/node-server/src/vite-proxy.ts
+++ packages/node-server/src/vite-proxy.ts
@@ -1,6 +1,7 @@
-import { default as fetch } from "node-fetch";
-import { leading, trailing } from "@zouloux/ecma-core";
-
+import { default as fetch } from "node-fetch"
+import { leading, trailing } from "@zouloux/ecma-core"
+import * as fs from "node:fs"
+import path from "path"
 
 interface IServeFrontEndOptions {
 	fastifyStatic	:any
@@ -9,44 +10,90 @@ interface IServeFrontEndOptions {
 	isDev			:boolean
 	upstream		:string
 	root			:string
+	// Do not deliver those static files
+	// Have to return false if fastify.get(requestPath, ...) exists somewhere in the app !
+	beforeFilter	?:( requestPath:string, request, reply) => boolean
 }
 
-export function serveFrontEnd ( options:IServeFrontEndOptions ) {
-	const { server, fastifyStatic, isDev, upstream, root, index = "/index.html" } = options
+export function serveFrontEnd(options:IServeFrontEndOptions) {
+	const { server, fastifyStatic, isDev, upstream, root, index = "/index.html", beforeFilter } = options
+	// Dev mode
 	if ( isDev ) {
-		server.addHook('onRequest', async (request, reply) => {
-			let requestPath = leading( request.url, true )
-			if ( requestPath === "/" )
+		// Get a request from vite server and proxy it to our client
+		async function replyProxiedRequest ( requestPath, reply ) {
+			if (requestPath === "/")
 				requestPath = index
 			// Try to relay any request to the dev server
-			const proxyURL = trailing( upstream, false ) + requestPath
+			const proxyURL = trailing(upstream, false) + requestPath
 			let proxyFetchRequest
 			try {
-				proxyFetchRequest = await fetch( proxyURL )
+				proxyFetchRequest = await fetch(proxyURL)
 			}
-			// Cannot contact proxiyed server, skip
-			catch (e) { return }
+				// Cannot contact proxiyed server, skip
+			catch (e) {
+				return false
+			}
 			// Fetch worked, but we got an HTTP error
 			// Continue with other route handlers
-			if ( !proxyFetchRequest.ok )
-				return
+			if (!proxyFetchRequest.ok)
+				return false
 			// Send back request to client with header
-			reply.headers( proxyFetchRequest.headers.raw() )
+			reply.headers(proxyFetchRequest.headers.raw())
 			// Read fetched content as blob
-			const blob:Blob = await proxyFetchRequest.blob()
+			const blob = await proxyFetchRequest.blob()
 			// Convert to array buffer
 			const arrayBuffer = await blob.arrayBuffer()
 			// Convert to buffer and send back to client
-			const buffer = Buffer.from( arrayBuffer )
-			reply.send( buffer )
-		})
+			const buffer = Buffer.from(arrayBuffer)
+			return reply.send(buffer)
+		}
+		// Proxy all requests to vite
+		server.addHook('onRequest', async (request, reply) => {
+			let requestPath = leading(request.url, true)
+			if ( beforeFilter && !beforeFilter(requestPath, request, reply) )
+				return
+			// Try to proxy from vite, otherwise continue
+			await replyProxiedRequest( requestPath, reply )
+		});
+		// Return a function to show a specific static file from vite dev
+		return async ( reply, requestPath = index ) => {
+			requestPath = leading(requestPath, true)
+			return await replyProxiedRequest( requestPath, reply )
+		}
 	}
+	// Production mode
 	else {
+		// Register fastify static with no serving ( we do the file serve bellow )
+		// To allow manual override of all static files.
 		server.register(fastifyStatic, {
 			root,
 			prefix: '/',
 			list: false,
-			dotfiles: 'deny'
+			dotfiles: 'deny',
+			// Those 2 are important to disable the static responder inside fastifyStatic
+			wildcard: false,
+			serve: false,
 		})
+		// Reply a static file in the root directory if it exists
+		async function replyStaticFile ( requestPath, reply ) {
+			const filePath = path.join( root, requestPath )
+			requestPath = leading( requestPath, false )
+			if ( !fs.existsSync( filePath ) || !fs.statSync(filePath).isFile() )
+				return false
+			return reply.sendFile( requestPath )
+		}
+		// Check all existing files in the root directory
+		server.addHook('onRequest', async (request, reply) => {
+			let requestPath = leading(request.url, true)
+			if ( beforeFilter && !beforeFilter(requestPath, request, reply) )
+				return
+			// Try to load and send file, otherwise continue
+			await replyStaticFile( requestPath, reply )
+		});
+		// Return a function to show a specific static file from compiled assets
+		return ( reply, requestPath = index ) => {
+			requestPath = leading(requestPath, true)
+			return replyStaticFile( requestPath, reply )
+		}
 	}
 }
